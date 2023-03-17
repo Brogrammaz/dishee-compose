68ffc71884fa18b459be9a0d9b14d76405a7c9bd
Karuchiu <karuchiu8@gmail.com>
2023-03-17 15:05:06 +0300

    Renamed form Payment to PaymentScreen

--- app/src/main/java/com/disheecompose/ui/PaymentScreen.kt
+++ app/src/main/java/com/disheecompose/ui/PaymentScreen.kt
@@ -1,7 +1,6 @@
-package com.disheecompose
+package com.disheecompose.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,10 +9,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.disheecompose.R
 import com.disheecompose.ui.theme.DisheecomposeTheme
 
 
